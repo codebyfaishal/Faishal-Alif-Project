aa783e7335a58858d27ac0f989f7e641ba832808
Faishal Alif Fadhillah <personalemailisal@gmail.com>
2023-07-14 05:32:39 +0700

    done add feature download

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TailwindProvider} from 'tailwind-rn';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -8,7 +8,45 @@ import utilities from './tailwind.json';
 import { Index } from './components/Index'
 import { Button } from 'react-native';
 
+import PDF from 'react-native-pdf';
+import ReactNativeBlobUtil from 'react-native-blob-util'
+import ListItem from './components/listItems';
+
 const Stack = createStackNavigator();
+
+ //download
+ const generatePDFUrl = () => {
+  const jsonDataString = JSON.stringify(ListItem, null, 2);
+  const pdfUrl = `data:application/pdf;base64,${btoa(jsonDataString)}`;
+  return pdfUrl;
+};
+
+const downloadPDF = () => {
+  const pdfUrl = generatePDFUrl();
+
+  ReactNativeBlobUtil.config({
+    fileCache: true,
+    appendExt: 'pdf',
+    addAndroidDownloads: {
+      useDownloadManager: true,
+      notification: true,
+      title: 'YourPDFFile',
+      description: 'Downloading PDF...',
+      mime: 'application/pdf',
+      mediaScannable: true,
+      path: ReactNativeBlobUtil.fs.dirs.DownloadDir + '/YourPDFFile.pdf',
+    },
+  })
+    .fetch('GET', pdfUrl)
+    .then((res) => {
+      console.log('PDF downloaded successfully:', res.path());
+    })
+    .catch((error) => {
+      console.error('Error downloading PDF:', error);
+    });
+};
+
+const pdfUrl = generatePDFUrl();
 
 export default function App() {
   return (
@@ -25,7 +63,7 @@ export default function App() {
   <Stack.Screen name=" " component={Index} options={{ 
   headerRight: (props) => ( // App Logo
   <Button
-  // onPress={onPressLearnMore}
+  onPress={downloadPDF}
   title="Download Rese"
 />
 ),
